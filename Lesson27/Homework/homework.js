10a1bc1a58d89084eeb81c10c93d00fc6657c67c
fazliddin-1dev <fazliddingoziboyev2@gmail.com>
2023-01-26 09:58:56 +0500

    first commit

---
+++ Lesson27/Homework/homework.js
@@ -0,0 +1,41 @@
+/* problem-1  Foydalanuvchi nechanchi yilda tug'ilganini kiritsin va u 20 yoki 21 asrda tug'ilgani consolega chiqarsin  */
+
+// var birthYear = prompt('what year were you born');
+// if (birthYear >= 1900 && birthYear < 2000) {
+//     console.log('you were born in the 20th century');
+// } else if (birthYear >= 2000 && birthYear < 2100) {
+//     console.log('you were born in the 21st century');
+// } else {
+//     console.log('you were born in another century');
+// }
+
+
+/* problem-2  Foydalunuvchidan X  va Y raqamini va X+Yni  hisoblab 
+agar to'g'ri bo'lsa "To'g'ri" noto'g'ri bo'lsa noto'g'ri */
+// let num1 = prompt('Enter number1');
+// let num2 = prompt('Enter number2');
+// let sum = prompt('num1 + num2');
+// let total = Number(num1) + Number(num2);
+// console.log(total)
+// if (sum == total) {
+//     console.log("True");
+// } else {
+//     console.log("False")
+// }
+
+
+/*  problem-3  Samalyot-$500, Mehmonxona-$250, Muzey-120yeuro  $1 = 11.000 so'm     1yeuro = 12.000 so'm */
+let country = prompt('Which country are you going to travel to?');
+let sum = prompt('How much money do you have?');
+let planeDollars = 500,
+hotelDollars = 250, 
+museumYeuro = 120;
+let uzbMoney = Number(planeDollars) * 11000;
+let uzbMoney2 = Number(hotelDollars) * 11000;
+let uzbMoney3 = Number(museumYeuro) * 12000;
+let allMoney = Number(uzbMoney) + Number(uzbMoney2) + Number(uzbMoney3);
+if (sum >= allMoney) {
+    console.log('Fazliddin, you have enough money');
+} else {
+    console.log('Fazliddin, save some more money and go on a trip next year');
+}
